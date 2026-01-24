dddf13c9426dc55313122a2f391964ff3fcdcaac
samrtchoice9th <smartchoice9thmile@gmail.com>
2026-01-24 21:49:44 +0530

    Update capacitor.config.ts
    url delete

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ahsanmasjid.sandakeeper',
   appName: 'masjid-sanda-keeper',
-  webDir: 'dist',
-  server: {
-    url: 'https://08a7ce71-5295-4660-9ad7-d18f9e431cda.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
+ 
 };
 
 export default config;
